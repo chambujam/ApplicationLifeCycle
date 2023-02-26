d67190b420639b45aa7ba99ac5ed50ebe9642bec
chambujam <104881419+chambujam@users.noreply.github.com>
2023-02-26 10:51:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/applicationlifecycle/SecondAcitivity.java
@@ -0,0 +1,73 @@
+package com.example.applicationlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondAcitivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_acitivity);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        Log.d("Message", "Second Activity onCreate");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Message", "Second Activity onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Message", " Second Activity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Message", "Second Activity onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Message", "Second Activity onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Message", "Second Activity onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Message", "Second Activity onRestart");
+    }
+}
